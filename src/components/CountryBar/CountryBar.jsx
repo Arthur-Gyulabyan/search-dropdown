f47653c20c28677a6eb3860af85f747232502a16
Arthur <gyulabyana@gmail.com>
2021-07-17 03:11:44 +0400

    feat: created SearchDropdown, CountriesBox and CountryBar compornents

---
+++ src/components/CountryBar/CountryBar.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './CountryBar.css';
+
+class CountryBar extends React.Component {
+    render() {
+        return (
+            <div className="country-bar">
+                <img src={this.props.flag} alt={this.props.countryName} className='country-image'/>
+                <p className="country-name">{this.props.countryName}</p>
+            </div>
+        )
+    }
+}
+
+export default CountryBar;
